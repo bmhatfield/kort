5cda30f395202c85b7036732173ec375ed0a25ac
Brian Hatfield <bmhatfield@gmail.com>
2023-07-23 16:01:49 -0400

    Add table / point list and allow selecting active

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -2,28 +2,39 @@
 const App = () => {
     const [tables, setTables] = React.useState();
     const [mode, setMode] = React.useState("new");
+    const [activeTableId, setActiveTableId] = React.useState("");
+
+    const [ptLabel, setPtLabel] = React.useState("");
 
     React.useEffect(() => {
         fetch("http://localhost:3000/tables").then(res => {
             return res.json();
         }).then(json => {
             setTables(json);
+
+            var newest = json.reduce(
+                function (a, b) {
+                    return Number(a.id) > Number(b.id) ? a : b;
+                }
+            );
+            setActiveTableId(newest.id);
         })
     }, []);
 
     function append(point) {
         const update = {
-            id: tables[tables.length - 1].id,
+            id: activeTableId,
             points: [point],
         };
 
         fetch("http://localhost:3000/table", { body: JSON.stringify(update), method: "PATCH" })
             .then(res => {
                 setTables(prev => {
-                    let last = prev[prev.length - 1];
-                    last.points.push(point);
-                    return [...prev.slice(0, -1), last];
+                    let active = prev.find(table => table.id === activeTableId)
+                    active.points.push(point);
+                    return [...prev];
                 });
+                setPtLabel("");
             });
     }
 
@@ -39,6 +50,8 @@ const App = () => {
         }).then(json => {
             update.id = json.id;
             setTables([...tables, update]);
+            setActiveTableId(json.id);
+            setPtLabel("");
         });
     }
 
@@ -63,14 +76,18 @@ const App = () => {
         };
     }
 
+    function canAppend() {
+        return activeTableId === undefined || activeTableId === ""
+    }
+
     return (
         <div>
             <Cartograph tables={tables} />
-            <div id="fcontainer">
-                <form id="points" onSubmit={handleSubmit}>
+            <div id="pointformcontainer">
+                <form id="pointform" onSubmit={handleSubmit}>
                     <div><label htmlFor="x">x</label><input id="x" name="x" type="number" min="-10000" max="10000" required /></div>
                     <div><label htmlFor="y">y</label><input id="y" name="y" type="number" min="-10000" max="10000" required /></div>
-                    <div><label htmlFor="label">label</label><input type="text" id="label" name="label" autoComplete="off" /></div>
+                    <div><label htmlFor="label">label</label><input type="text" id="label" name="label" autoComplete="off" value={ptLabel} onChange={(e) => setPtLabel(e.target.value)} /></div>
                     <div><label htmlFor="biome">biome</label>
                         <select id="biome" name="biome" defaultValue={"ocean"}>
                             <option value="">None</option>
@@ -92,10 +109,11 @@ const App = () => {
                             <option value="track">Track</option>
                         </select>
                     </div>
-                    <input type="submit" value="Append" className="sub-mode" onClick={(() => setMode("append"))} />
+                    <input type="submit" value="Append" className="sub-mode" disabled={canAppend()} onClick={(() => setMode("append"))} />
                     <input type="submit" value="New" className="sub-mode" style={{ marginRight: 25 }} onClick={(() => setMode("new"))} />
                 </form>
             </div>
+            <TableList tables={tables} activeTableId={activeTableId} setActiveTableId={setActiveTableId} />
         </div>
     )
 }
---
+++ static/js/list.jsx
@@ -0,0 +1,54 @@
+const TableList = ({ tables, activeTableId, setActiveTableId }) => {
+    if (tables === undefined) {
+        return
+    }
+
+    function listliClick(tableId) {
+        if (activeTableId === tableId) {
+            setActiveTableId("");
+            return
+        }
+
+        setActiveTableId(tableId);
+    }
+
+    let newest = tables.reduce((a, b) => {
+            return Number(a.id) > Number(b.id) ? a : b;
+        }
+    );
+
+    let items = tables.filter(table => table.points.length > 1 || table.id === newest.id).map(table => {
+        let showPoints = (activeTableId === table.id);
+        let points;
+
+        // Get list of points, if active for this table
+        if (showPoints) {
+            points = table.points.map((point, i) => {
+                let label = (point.label !== undefined) ? <div>{point.label}</div> : null;
+                return (
+                    <li className={"pointitem listli"} key={i}>
+                        <div>{label}</div>
+                        <div>({point.x}, {point.y})</div>
+                    </li>
+                )
+            })
+        }
+
+        // Return table
+        return (
+            <li className={"listli"} key={table.id} onClick={()=>{listliClick(table.id)}}>
+                <div className={"tableitem"}>
+                    <div className={"tablesize"}>{table.points.length}</div>
+                    <div>{table.kind}</div>
+                </div>
+                {showPoints && <ul className={"pointset"}>{points}</ul>}
+            </li>
+        )
+    });
+
+    return (
+        <ul id="list">
+            {items}
+        </ul>
+    )
+}
