d937a45dbd13d1bc5558fbfd44aa0b5d9ff47191
Brian Hatfield <bmhatfield@gmail.com>
2023-07-30 12:34:21 -0400

    Tighten route grouping

--- app/router.go
+++ app/router.go
@@ -24,52 +24,47 @@ func (r Router) Mux() *chi.Mux {
 func NewRouter(svc *Service) *Router {
 	r := chi.NewRouter()
 
+	// Compress all routes
 	r.Use(
 		middleware.Compress(5),
 	)
 
-	r.Route("/poly", func(r chi.Router) {
-		r.Use(middleware.Logger, svc.auth)
+	// Authenticated / logged routes
+	r.Group(func(r chi.Router) {
+		r.Use(
+			middleware.Logger,
+			svc.auth,
+		)
 
-		r.Get("/{polyId}", svc.getPoly)
+		r.Route("/poly", func(r chi.Router) {
+			r.Get("/{polyId}", svc.getPoly)
+			r.Post("/", svc.newPoly)
+			r.Delete("/{polyId}", svc.deletePoly)
+		})
 
-		r.Post("/", svc.newPoly)
+		r.Route("/polys", func(r chi.Router) {
+			r.Get("/", svc.getPolys)
+		})
 
-		r.Delete("/{polyId}", svc.deletePoly)
+		r.Route("/user", func(r chi.Router) {
+			r.Get("/{userId}", svc.getUser)
+		})
+
+		r.Route("/users", func(r chi.Router) {
+			r.Get("/", svc.getUsers)
+		})
+
+		r.Route("/point", func(r chi.Router) {
+			r.Delete("/{polyId}/{offset}", svc.deletePoint)
+		})
+
+		r.Route("/points", func(r chi.Router) {
+			r.Put("/{polyId}", svc.addPoints)
+		})
 	})
 
-	r.Route("/polys", func(r chi.Router) {
-		r.Use(middleware.Logger, svc.auth)
-
-		r.Get("/", svc.getPolys)
-	})
-
-	r.Route("/user", func(r chi.Router) {
-		r.Use(middleware.Logger, svc.auth)
-
-		r.Get("/{userId}", svc.getUser)
-	})
-
-	r.Route("/users", func(r chi.Router) {
-		r.Use(middleware.Logger, svc.auth)
-
-		r.Get("/", svc.getUsers)
-	})
-
-	r.Route("/point", func(r chi.Router) {
-		r.Use(middleware.Logger, svc.auth)
-
-		r.Delete("/{polyId}/{offset}", svc.deletePoint)
-	})
-
-	r.Route("/points", func(r chi.Router) {
-		r.Use(middleware.Logger, svc.auth)
-
-		r.Put("/{polyId}", svc.addPoints)
-	})
-
-	fs := http.FileServer(http.Dir("static"))
-	r.Mount("/", fs)
+	// File server
+	r.Mount("/", http.FileServer(http.Dir("static")))
 
 	return &Router{mux: r, svc: svc}
 }
